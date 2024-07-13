b9d5fc3051055438153a457d3c991b7090b0eeed
iSitanshu <Sitanshumishra18@gmail.com>
2024-07-14 00:30:44 +0530

    Ready Page

--- frontend/src/Components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -4,7 +4,7 @@ import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div className='explore-menu' id='eplore-menu'>
+    <div className='explore-menu' id='explore-menu'>
         <h1>Explore our menu</h1>
         <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
         <div className="explore-menu-list">
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 import "./Navbar.css"
 
 const Navbar = () => {
@@ -9,10 +10,10 @@ const Navbar = () => {
     <div className='Navbar'>
       <img src={assets.logo} alt=""  className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
-        <li onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
-        <li onClick={()=>setMenu("Mobile-App")} className={menu==="Mobile-App"?"active":""}>Mobile App</li>
-        <li onClick={()=>setMenu("Contact-us")} className={menu==="Contact-us"?"active":""}>Contact us</li>
+        <Link to='/' onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link>
+        <a href="#explore-menu" onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</a>
+        <a href="#app-download" onClick={()=>setMenu("Mobile-App")} className={menu==="Mobile-App"?"active":""}>Mobile App</a>
+        <a href="#footer" onClick={()=>setMenu("Contact-us")} className={menu==="Contact-us"?"active":""}>Contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
